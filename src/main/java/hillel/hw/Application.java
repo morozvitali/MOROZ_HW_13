02a037877d14716aa55f38ad5db5b42f2c4e7969
morozvitali <morozvitali99@gmail.com>
2023-02-28 23:17:30 +0200

    bug fix # delete add files

--- src/main/java/hillel/hw/Application.java
+++ src/main/java/hillel/hw/Application.java
@@ -1,56 +1,36 @@
 package src.main.java.hillel.hw;
 
 public class Application {
-        public static void main(String[] args) {
-//        FileData fileData0 = new FileData("txt0.txt", 672, "./src/main/java/resources/resourses.0/");
-//        FileData fileData1 = new FileData("txt1.txt", 4322, "./src/main/java/resources/resourses.1/");
-//        FileData fileData2 = new FileData("txt2.txt", 466722, "./src/main/java/resources/resourses.1/");
-//        FileData fileData3 = new FileData("txt3.txt", 45222, "./src/main/java/resources/2/");
-//        FileData fileData4 = new FileData("txt4.txt", 28762, "./src/main/java/resources/2/");
-//        FileData fileData10 = new FileData("txt10.txt", 22987, "./src/main/java/resources/2/");
-//        FileData fileData11 = new FileData("txt11.txt", 2865, "./src/main/java/resources/2/");
-//        FileData fileData12 = new FileData("txt12.txt", 26582, "./src/main/java/resources/2/");
-//        FileData fileData13 = new FileData("txt12.txt", 26582, "./src/main/java/resources/2/");
-//        FileData fileData14 = new FileData("txt12.txt", 26582, "./src/main/java/resources/2/");
-
+    public static void main(String[] args) {
         FileNavigator fileNavigator = new FileNavigator();
 
-        fileNavigator.add(".\\resourses\\0\\txt0.txt");
-        fileNavigator.add(".\\resourses\\1\\txt1.txt");
-        fileNavigator.add(".\\resourses\\1\\txt2.txt");
-        fileNavigator.add(".\\resourses\\2\\txt3.txt");
-        fileNavigator.add(".\\resourses\\2\\txt4.txt");
-//        fileNavigator.add(".\\resourses\\2\\txt10.txt");
-//        fileNavigator.add(".\\resourses\\2\\txt11.txt");
-//        fileNavigator.add(".\\resourses\\2\\txt12.txt");
-//
-//        fileNavigator.add(".\\resours\\2\\txt12.txt");
-
-
-        // додаємо по 2 разу
-//        fileNavigator.add(".\\resourses\\2\\txt12.txt");
-//        fileNavigator.add(".\\resourses\\2\\txt12.txt");
-        // додаємо директорію
+        //Додаємо файли у каталог
+        fileNavigator.add(".\\resourses\\0\\");
+        fileNavigator.add(".\\resourses\\1\\");
         fileNavigator.add(".\\resourses\\2\\");
 
+        // додаємо по 2 разу
+        fileNavigator.add(".\\resourses\\2\\txt12.txt");
+        fileNavigator.add(".\\resourses\\2\\txt12.txt");
 
-        //спроба додати дублікат
-//        fileNavigator.add(fileData13);
-//        fileNavigator.add(fileData14);
+        // видаляємо папку, це знищить всі папки і файли 1,2,3
+//        fileNavigator.remove(".\\resourses\\1");
+//        fileNavigator.remove(".\\resourses\\2");
+//        fileNavigator.remove(".\\resourses\\0");
+
+            // спроба видалити те чого не існує
+        fileNavigator.remove(".\\res\\");
+
 
         // метод що шукає файли у окремій папці
-//        System.out.println(fileNavigator.find("./src/main/java/resources/2/"));
+        System.out.println(fileNavigator.find(".\\resourses\\2"));
 
-        // видаляємо папку
-//        fileNavigator.remove("./src/main/java/resources/2/");
-//        System.out.println(fileNavigator.find("./src/main/java/resources/2/"));
 
         // сортуємо за розміром
         System.out.println(fileNavigator.sortBySize());
 
+
         // фільтруємо за розміром
-//        System.out.println(fileNavigator.filterBySize("./src/main/java/resources/resourses.1/", 100000));
-
-
+        System.out.println(fileNavigator.filterBySize(".\\resourses\\2", 100000));
     }
 }
--- src/main/java/hillel/hw/FileNavigator.java
+++ src/main/java/hillel/hw/FileNavigator.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 public class FileNavigator {
     private Map<String, List<FileData>> fileMap;
+
     public FileNavigator() {
         fileMap = new HashMap<>();
     }
@@ -15,7 +16,6 @@ public class FileNavigator {
 
         String path = fileData.getPath();
         String name = fileData.getName();
-       /////////////////// wtf  if (fileData.getPath().endsWith("\\")) return;
 
         List<FileData> files = fileMap.computeIfAbsent(path, k -> new ArrayList<>());
 
@@ -24,32 +24,32 @@ public class FileNavigator {
 
         if (!hasDuplicateFile) {
             files.add(fileData);
-        }
-        else {
+        } else {
             System.out.println("Спроба додати файл з тим самим ім'ям");
             System.out.println("Помилка: файл " + fileData.getName() + " вже існує по вказаному шляху " + fileData.getPath());
         }
     }
+
     public void add(String path) {
         File file = new File(path);
-        if (file.exists()) {
+        if (file.exists() && !file.isDirectory()) {
             String directoryPath = file.getParent();
             FileData fileData = new FileData(file.getName(), file.length(), directoryPath);
             addToHashMap(fileData);
         } else if (file.isDirectory()) {
-                    for (File f : file.listFiles()) {
-                        FileData fileData = new FileData(f.getName(), f.length(), f.getPath());
-                        addToHashMap(fileData);
-                    }
-                } else {
-            System.out.println("Шлях " + path +" не існує");
+            for (File f : file.listFiles()) {
+                if (!f.isFile()) continue;
+                FileData fileData = new FileData(f.getName(), f.length(), f.getParent());
+                addToHashMap(fileData);
             }
+        } else {
+            System.out.println("Шлях " + path + " не існує");
         }
+    }
 
 
-
-        public List<FileData> find(String path) {
-            System.out.println("Шукаємо файли за шляхом: метод find() ");
+    public List<FileData> find(String path) {
+        System.out.println("Шукаємо файли за шляхом: метод find() ");
 
         if (fileMap.containsKey(path)) {
             return fileMap.get(path);
@@ -67,8 +67,27 @@ public class FileNavigator {
     }
 
     public void remove(String path) {
-        System.out.println("Видаляємо шлях: метод remove() ");
-        fileMap.remove(path);
+        File f = new File(path);
+
+
+        if (f.exists() && f.isFile()) {
+            f.delete();
+            System.out.println("Файл за посиланням " + path + " видалено");
+            fileMap.remove(path);
+
+            System.out.println("Видаляємо файл " + f.getName() + " з таблиці файлів: метод remove() ");
+        } else if (f.exists() && !f.isFile()) {
+            System.out.println("Видаляємо директорію з файлами");
+            for (File file : f.listFiles()) {
+                System.out.println("Спочатку видалимо файли в середині папки, якщо вони є, спроба видалити" + file);
+                file.delete();
+            }
+            f.delete();
+
+            System.out.println("Спроба видалити директорію з переліку");
+            fileMap.remove(path);
+        } else
+            System.out.println("Файл за посиланням " + path + " не існує");
     }
 
     public List<FileData> sortBySize() {
